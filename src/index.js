e604548aabecd09fa307b7c5560ce4e4fcc3a12a
Raboro <raborogit@gmail.com>
2023-04-29 12:52:02 +0200

    added getAllFiles

--- src/index.js
+++ src/index.js
@@ -2,4 +2,37 @@
 
 'use strict';
 
-console.log('language-detection-CLI-tool');
+const fs = require('fs');
+
+console.log(getAllFiles('.', []));
+
+function getAllFiles(dir, fileList) {
+    if (isDirectory(dir)) {
+        const files = fs.readdirSync(dir);
+        fileList = iterateOverFiles(files, dir, fileList);
+        return fileList;
+    }
+    fileList.push(dir);
+    return fileList;
+}
+
+function isDirectory(dir) {
+    return fs.statSync(dir).isDirectory();
+}
+
+function iterateOverFiles(files, dir, fileList) {
+    files.forEach((file) => {
+        if (!needToBeIgnored(file)) {
+            if (isDirectory(dir + '/' + file)) {
+                fileList = getAllFiles(dir + '/' + file, fileList);
+            } else {
+                fileList.push(file);
+            }
+        }
+    });
+    return fileList;
+}
+
+function needToBeIgnored(file) {
+    return file.endsWith('git') || file.endsWith('node_modules') || file.endsWith('idea');
+}
